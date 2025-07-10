6da496de4a8c3d988866129e38281b1f87cf38d6
Franco Finazzi <francofinazzi@MacBook-Air-de-Franco.local>
2025-07-10 20:24:28 -0300

    avance de Backend en general.

--- Backend/src/api.js
+++ Backend/src/api.js
@@ -5,8 +5,14 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-const { getAllusuarios } = require('./gestiondetareas');
+const { 
+    getAllusuarios,
+    getOneusuario,
+    Createusuario,
+    Deleteusuario,
+} = require('./gestiondetareas');
 
+//health route
 app.get('/api/health', (req, res) => {
     res.json({status: "Levantado"});
 });
@@ -19,13 +25,61 @@ app.get('/api/usuarios', async (req, res) => {
 });
 
 //get one usuarios
-// app.get('/api/usuarios/:id', (req, res) => {
-//     //res.json({status: 'Ok'})
-// });
+app.get('/api/usuarios/:id', async (req, res) => {
+    const usuario = await getOneusuario(req.params.id);
+    if (!usuario) {
+        return res.status(404).json({error: 'Usuario no encontrado'});
+    }
+    res.json(usuario);
+ });
 
 //insert usuario
+/* esto es para probar el post a ver si funca bien jeje
+   curl --header "Content-Type: application/json" \
+   --request POST \
+   --data '{"nombre":"Franco","apellido":"Finazzi","rol":"SDR","avatar":1,"mail":"franfinazzi@gmail.com"}' \
+   http://localhost:3000/api/usuarios */
+
+app.post('/api/usuarios', async (req, res) => {
+
+    if (!req.body.nombre ||
+        !req.body.nombre ||
+        !req.body.apellido ||
+        !req.body.rol ||
+        !req.body.avatar ||
+        !req.body.mail) {
+            return res.status(400).json({error: 'Se requieren datos'});
+        }
+    
+    const usuario = await Createusuario(
+        req.body.nombre,
+        req.body.apellido,
+        req.body.rol,
+        req.body.avatar,
+        req.body.mail,
+    );
+
+    if (!usuario) {
+        return res.status(500).json({error: 'Error al crear usuario'});
+    }
+    res.json({usuario});
+});
+
+//delete usuario
+app.delete('/api/usuarios/:id', async (req, res) => {
+
+    const usuario = await Deleteusuario(req.params.id);
+
+    if (!personaje) {
+        return res.status(404).json({error: 'Ususario id: ' + req.params.id + ' no encontrado'});
+    }
+    res.json({status: 'Ok', id: usuario});
+});
 
 //update usuario
+app.put('/api/usuarios', (req, res) => {
+    res.json({status: 'Ok'})
+});
 
 
 //Proyectos
--- Backend/src/gestiondetareas.js
+++ Backend/src/gestiondetareas.js
@@ -14,6 +14,41 @@ async function getAllusuarios() {
     return result.rows;
 }
 
+async function getOneusuario(id) {
+    const result = await dbclient.query('SELECT * FROM usuarios WHERE id = $1 LIMIT 1', [id]);
+    return result.rows[0];
+}
+
+async function Createusuario(
+    nombre,
+	apellido,
+	rol,
+	avatar,
+	mail,
+) {
+    const result = await dbclient.query(
+        'INSERT INTO usuarios(nombre, apellido, rol, avatar, mail) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+        [nombre, apellido, rol, avatar, mail]
+    );
+
+    if (result.rowCount === 0) {
+        return undefined
+    }
+        return result.rows[0];
+}
+
+async function Deleteusuario(id) {
+    const result = await dbclient.query('DELETE * FROM usuarios WHERE id = $1', [id]);
+
+    if (result.rowCount === 0) {
+        return undefined;
+    }
+    return id;
+}
+
 module.exports = {
-    getAllusuarios 
+    getAllusuarios,
+    getOneusuario,
+    Createusuario,
+    Deleteusuario,
 };
